309a19a2bfce98a5cac2582afda7cf894aa4a455
chris <1500780323@qq.com>
2021-10-11 17:16:05 +0800

    增加深兰python基础

---
+++ pythonbase/homework/20211010/loginsystem.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+user = ['root', 'test']
+passwds = ['123', '456']
+
+
+username = input("请输入您的用户名\n")
+
+print(user.index(username))
+#for username in user:
+    #print(username)
+    #break
